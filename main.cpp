d0ce20cf6369f9c34e7ba2419c623ecde6e7aa50
Howard <xly931@126.com>
2015-09-16 18:29:59 +0800

    Revert "Revert "TRY""
    This reverts commit f72d16cf3b67ac3c5734c6cff2cb2b21325893e7.

--- main.cpp
+++ main.cpp
@@ -11,5 +11,13 @@ int main()
   cout<<"no 3 version"<<endl;
   cout<<fun_string("AAA","BBB")<<endl;
   system("pause");
+  TRY 
+  {
+  }
+  CATCH (CMemoryException, e)
+  {
+  	
+  }
+  END_CATCH
   return 0;
 }
