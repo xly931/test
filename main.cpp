12a5be78743a369b023638aba8a9682e049d2a9c
Howard <xly931@126.com>
2015-09-16 15:28:44 +0800

    add win project
    add  windows project with vs2012

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
+#include <stdlib.h>
 using namespace std;
+
 
 int main()
 {
   cout<<"no 1 version"<<endl;
   cout<<"no 2 version"<<endl;
   cout<<"no 3 version"<<endl;
+  system("pause");
+  return 0;
 }
